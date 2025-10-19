204e7ae9af8fe1af168878286c9709f2413e8c74
srikanth <srikanthbg.gcp1@gmail.com>
2025-10-19 14:35:22 +0000

    added print stmt to test files

--- tests/test_data_validation.py
+++ tests/test_data_validation.py
@@ -4,6 +4,7 @@ from src.evaluate import load_data
 
 class TestDataValidation(unittest.TestCase):
     def test_schema_and_ranges(self):
+        print("inside test_data_validation.py")
         df = load_data("data.csv")
         basic_schema_checks(df)  # raises AssertionError if invalid
 
