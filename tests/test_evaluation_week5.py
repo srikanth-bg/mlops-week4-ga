f2389f0c2f176e6393d8aaf5bed0b1b147fb20ef
srikanth <srikanthbg.gcp1@gmail.com>
2025-10-23 18:17:53 +0000

    added print stmt to test_evaluation_week5.py

--- tests/test_evaluation_week5.py
+++ tests/test_evaluation_week5.py
@@ -5,6 +5,7 @@ class TestEvaluation(unittest.TestCase):
     def test_accuracy_threshold(self):
         print("Evaluation begin")
         metrics = evaluate(data_path="data.csv", target_col="species", metric_name="accuracy", experiment_name="IRIS classifier experiment: Week5GA")
+        print(f"Metrics: {metrics}")
         self.assertGreaterEqual(metrics["accuracy"], 0.80, "Accuracy below expected threshold")
         print("Evaluation completed")
 
